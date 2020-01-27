3a95c9acc52468ff535d20b80c937c7c172152a8
Simon <ekbhard@gmail.com>
2020-01-27 22:30:51 +0500

    primitive login

---
+++ src/main/java/com/skb/SpringSKBProject/controllres/RegistrationController.java
@@ -0,0 +1,45 @@
+package com.skb.SpringSKBProject.controllres;
+
+import com.skb.SpringSKBProject.domain.User;
+import com.skb.SpringSKBProject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String,Object> model,
+                          @RequestParam("surname") String surname,
+                          @RequestParam("name") String name,
+                          @RequestParam("patronymic") String patronymic){
+        if (user.getPassword().equals("") || user.getUsername().equals("")){
+            model.put("message","Null login and password");
+            return "registration";
+        }
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if (userFromDb != null){
+            model.put("message","User founded");
+            return "registration";
+        }
+        user.setActive(true);
+        user.setName(name);
+        user.setPatronymic(patronymic);
+        user.setSurname(surname);
+        userRepository.save(user);
+        return "redirect:/registration";
+    }
+}
---
+++ src/main/java/com/skb/SpringSKBProject/domain/User.java
@@ -0,0 +1,122 @@
+package com.skb.SpringSKBProject.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
+@Entity
+@Table(name = "usr")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, length = 40)
+    private String username;
+
+    @NotNull
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @NotNull
+    @Column(nullable = false, length = 60)
+    private String surname;
+
+    @NotNull
+    @Column(nullable = false, length = 60)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false, length = 60)
+    private String patronymic;
+
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+}
